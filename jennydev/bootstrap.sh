de092a629e011de4e9e98520546e18742c3f8c5a
Phil <philporada@gmail.com>
2014-11-02 16:11:41 -0500

    Changed up some configs

---
+++ jennydev/bootstrap.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+echo
+echo ">>> Installing some useful stuff"
+sudo apt-get -y upgrade
+sudo apt-get install -y build-essential git git-core
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+
+echo
+echo ">>> Installing Yeoman"
+sudo npm install -g yo
+
+echo
+echo ">>> Installing Grunt"
+sudo npm install -g grunt
+
+echo
+echo ">>> Installing Bower"
+sudo npm install -g bower
+
+echo
+echo ">>> Installing Composer"
+sudo npm install -g composer
+
+echo
+echo ">>> Installing RVM"
+curl -L https://get.rvm.io | bash -s stable
+source /home/vagrant/.rvm/scripts/rvm
+echo "source /home/vagrant/.rvm/scripts/rvm" >> /home/vagrant/.bashrc
+echo "gem: --no-ri --no-rdoc" > /home/vagrant/.gemrc
+sudo su - vagrant -c "rvm get stable --auto"
+sudo su - vagrant -c "rvm reload"
+
+echo
+echo ">>> Creating the /usr/local/www/sites/ directory"
+sudo mkdir /usr/local/www/jenny
+sudo ln -s /vagrant /usr/local/www/sites/jenny
+
+echo
+echo ">>> Add Vagrant to www-data group"
+sudo usermod -a -G www-data vagrant
+
+echo
+echo ">>> Installing *.jenny.dev self-signed SSL"
+
+SSL_DIR="/etc/ssl/jenny.dev"
+DOMAIN="*.jenny.dev"
+PASSPHRASE="jenny"
+
+SUBJ="
+C=US
+ST=Michigan
+O=DearbornHeights
+localityName=Detroit
+commonName=$DOMAIN
+organizationalUnitName=Jenny
+emailAddress=webmaster@jenny.dev
+"
+
+sudo mkdir -p "$SSL_DIR"
+sudo openssl genrsa -out "$SSL_DIR/jenny.dev.key" 1024
+sudo openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "$SSL_DIR/jenny.dev.key" -out "$SSL_DIR/jenny.dev.csr" -passin pass:$PASSPHRASE
+sudo openssl x509 -req -days 365 -in "$SSL_DIR/jenny.dev.csr" -signkey "$SSL_DIR/jenny.dev.key" -out "$SSL_DIR/jenny.dev.crt"
+
+echo
+echo ">>> Enabling vhost alias"
+sudo a2enmod vhost_alias
+
+echo
+echo ">>> Adding jenny.dev config"
+sudo curl https://raw.githubusercontent.com/pgporada/Vagrants/master/jennydev/apache/jenny.dev.conf  > /etc/apache2/sites-available/jenny.dev.conf
+sudo a2ensite jenny.dev
+sudo service apache2 restart
+
+echo
+echo ">>> Removing the fileMode from git tracked files"
+sudo su - vagrant -c 'git config --global core.fileMode false'
+
+echo
+echo ">>> Installing dotfiles and bashit"
+git clone https://github.com/revans/bash-it.git /home/vagrant/.bash_it
+sudo curl https://raw.githubusercontent.com/pgporada/Vagrants/master/jennydev/dotfiles/.bash_profile > /home/vagrant/.bash_profile
+
+echo
+echo ">>> Adding Node path to .bashrc"
+echo "export NODE_PATH=$NODE_PATH:/home/vagrant/npm/lib/node_modules" >> /home/vagrant/.bashrc && source /home/vagrant/.bashrc
+echo
+echo ">>> Adding vendor/bin to your path"
+echo 'export PATH=vendor/bin:$PATH' >> /home/vagrant/.bashrc
+echo
+echo ">>> Removing StrictHostKey Config and adding to local Vagrant Known Hosts"
+rm /root/.ssh/config
+sudo su - vagrant -c 'ssh-keyscan -H github.com >> /home/vagrant/.ssh/known_hosts'
+sudo su - vagrant -c 'ssh-keyscan -H bitbucket.org >> /home/vagrant/.ssh/known_hosts'
+
+echo
+echo ">>> Installing SASS CSS Importer"
+sudo su - vagrant -c 'gem install --pre sass-css-importer'
+
+echo
+echo ">>> Installing newest Virtual Box Additions"
+curl -O  http://download.virtualbox.org/virtualbox/4.3.16/VBoxGuestAdditions_4.3.16.iso
+sudo mkdir /media/VBoxGuestAdditions
+sudo mount -o loop,ro VBoxGuestAdditions_4.3.16.iso /media/VBoxGuestAdditions
+yes | sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
+rm VBoxGuestAdditions_4.3.16.iso
+sudo umount /media/VBoxGuestAdditions
+sudo rmdir /media/VBoxGuestAdditions
